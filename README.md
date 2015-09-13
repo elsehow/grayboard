a6bc3d03667ad5ceb7df9ed5ab77f9e517506501
elsehow <yes@cosmopol.is>
2015-09-12 20:03:57 -0700

    ability to save HTML with drawings on it.

---
+++ README.md
@@ -0,0 +1,11 @@
+# grayboard
+
+a whiteboard for graybeards
+
+## development
+
+clone + `npm install`
+
+simply `npm run-script build-dev` while developing
+
+to make it all one nice distributeable html file, `npm run-script build-production`, and take the generated app.html file and give it to your friends
--- app/entry.js
+++ app/entry.js
@@ -1,28 +1,39 @@
-var paper = require('paper')
-  , canvas = document.getElementById("canvas")
+var paper   = require('paper')
+  , $       = require('jquery')
+  , canvas  = document.getElementById("canvas")
+  , $svg = $("#svg")
 
 function setCanvasSize () {
   canvas.width  = window.innerWidth;
   canvas.height = window.innerHeight;
 }
 
-paper.install(window);
+var setup = function () {
 
-window.addEventListener('resize', setCanvasSize)
-
-window.onload = function() {
+  // setup paper
+  paper.install(window) // this and next line used to be outside setup()
+  window.addEventListener('resize', setCanvasSize)
   setCanvasSize()
-  paper.setup('canvas');
+  paper.setup('canvas')
+
+  // try to get old shit from dom
+  var path = new Path() 
+  path.importSVG($svg[0])
+
   // Create a simple drawing tool:
-  var tool = new Tool();
-  var path;
+  var tool = new Tool()
   // Define a mousedown and mousedrag handler
-  tool.onMouseDown = function(event) {
-  	path = new Path();
-  	path.strokeColor = 'black';
-  	path.add(event.point);
+  tool.onMouseDown = function (event) {
+  	path = new Path()
+  	path.strokeColor = 'black'
+  	path.add(event.point)
   }
-  tool.onMouseDrag = function(event) {
-  	path.add(event.point);
+  tool.onMouseDrag = function (event) {
+  	path.add(event.point)
+  }
+  tool.onMouseUp = function (_) {
+    $svg.append(path.exportSVG({asString: false}))
   }
 }
+
+$(document).on('ready', setup)
